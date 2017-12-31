be8b62034564ee72168bcd25f02225d478c9906b
Vaidotas Staugaitis <vstaugaitis@sanityassembly.com>
2017-12-31 15:24:00 -0600

    Implemented methods for printing reports in APP.py

--- App.py
+++ App.py
@@ -20,6 +20,53 @@ def most_requested_articles():
     result = cursor.fetchall()
     connection.close()
     
-    ## return results
-    return result
+    ## return results       
+    for i in result:
+        print("{0} -- {1}".format(i[0], i[1]))
+        
 
+def most_read_authors():
+    #The SQL query which return the data
+    query = '''
+    SELECT authors.name, count(authors.name)
+    FROM 
+        (articles JOIN article_to_log ON id = article_to_log.article_id)
+        JOIN authors
+        ON author = authors.id    
+    GROUP BY authors.name
+    '''
+
+    #Execute the query
+    connection = psycopg2.connect(database=DBNAME)
+    cursor = connection.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    connection.close()
+    
+    ## format and print results       
+    for i in result:
+        print("{0} -- {1}".format(i[1], i[0]))
+
+def request_errors_by_day():
+    #The SQL query which return the data
+    query = '''
+    SELECT  status, 
+        count(status) AS NumberOfErrors,
+        --date_trunc('day', time) AS day
+        log.time::date AS day
+    FROM log
+    WHERE status != '200 OK'
+    GROUP BY status, day
+    ORDER BY NumberOfErrors DESC
+    '''
+
+    #Execute the query
+    connection = psycopg2.connect(database=DBNAME)
+    cursor = connection.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    connection.close()
+    
+    ## format and print results       
+    for i in result:
+        print('Error count: {0} -- day: {1}'.format(i[1], i[2]))
