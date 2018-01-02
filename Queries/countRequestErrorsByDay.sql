4c3b846f4c8f7408972b7dbb910feec4b9fcb30e
Vaidotas Staugaitis <vstaugaitis@sanityassembly.com>
2018-01-01 18:18:59 -0600

    Added a view which displays arror code quantities by day

--- Queries/countRequestErrorsByDay.sql
+++ Queries/countRequestErrorsByDay.sql
@@ -1,8 +1,16 @@
-SELECT  status, 
-        count(status) AS NumberOfErrors,
-        --date_trunc('day', time) AS day
-        log.time::date AS day
-    FROM log
-    WHERE status != '200 OK'
-    GROUP BY status, day
-    ORDER BY NumberOfErrors DESC
+SELECT  
+        log.time::date AS day,
+        sum(CASE WHEN log.status = '200 OK' THEN 0 ELSE 1 END) AS Errors,
+        count(*) AS requests,
+        log.status,
+        totalRequests
+    FROM    log 
+            JOIN 
+                (SELECT count(status) AS totalRequests,
+                --sum(CASE WHEN log.status = '200 OK' THEN 0 ELSE 1 END) AS Errors,
+                log.time::date as dayx
+                FROM log
+                GROUP BY dayx) as x 
+            ON x.dayx = log.time::date 
+    GROUP BY status, day, totalRequests
+--ORDER BY NumberOfErrors DESC
