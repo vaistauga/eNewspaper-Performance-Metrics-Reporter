530984ffcde479400deffe74f46c333507cf1681
Vaidotas Staugaitis <vstaugaitis@sanityassembly.com>
2018-01-01 18:20:52 -0600

    Deleted the status code query file, because it's wrong and will be rewriten.

--- Queries/most_requested_articles.sql
+++
@@ -1,6 +0,0 @@
--- Query used to get the three most requested viwed articles from the website
-select count(article_id) AS views, articles.title, article_id
-    FROM article_to_log JOIN articles ON (id = article_id)
-    GROUP BY article_id, articles.title
-    ORDER BY views DESC
-    LIMIT 3
